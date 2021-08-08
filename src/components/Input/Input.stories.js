b6c7aaa529ecdccca10b00a608893fe0fad9d7d4
azharimm <azhari.marzan@gmail.com>
2021-08-08 12:15:01 +0700

    chore: input component & stories

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Input from './Input'
+
+export default {
+    title: 'Form/Input',
+    component: Input,
+}
+
+export const Small = () => <Input size='small' placeholder='small input' />
+export const Medium = () => <Input size='medium' placeholder='medium input' />
+export const Large = () => <Input size='large' placeholder='large input' />
